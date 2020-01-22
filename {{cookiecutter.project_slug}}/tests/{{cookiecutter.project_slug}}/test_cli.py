993669eac9e500f8551e18c31940ddbdf189264c
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-01-22 11:00:55 +0100

    turn CLI test function into four-method class

--- {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}/test_cli.py
@@ -11,18 +11,29 @@ from {{ cookiecutter.project_slug }} import cli
 pytest
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "test_cli_project.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert "Show this message and exit." in help_result.output
-    dry_run_result = runner.invoke(cli.main, ["-n"])
-    assert dry_run_result.exit_code == 0
-    assert "Is dry run" in dry_run_result.output
-    version_result = runner.invoke(cli.main, ["-V"])
-    assert version_result.exit_code == 0
-    assert cli.__version__ in version_result.output
+class TestCLI:
+    """Test the command line interface."""
+
+    def call(self, args=None):
+        runner = CliRunner()
+        return runner.invoke(cli.main, args)
+
+    def test_default(self):
+        result = self.call()
+        assert result.exit_code == 0
+        assert "test_cli_project.cli.main" in result.output
+
+    def test_help(self):
+        result = self.call(["--help"])
+        assert result.exit_code == 0
+        assert "Show this message and exit." in result.output
+
+    def test_version(self):
+        result = self.call(["-V"])
+        assert result.exit_code == 0
+        assert cli.__version__ in result.output
+
+    def test_dry_run(self):
+        result = self.call(["-n"])
+        assert result.exit_code == 0
+        assert "Is dry run" in result.output
