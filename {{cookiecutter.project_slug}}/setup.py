8623472a3a038cfca46d8b4b7619f6d102f93d4f
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-01-31 09:21:06 +0100

    update Makefile from pyflexplot
    add pyproject.toml and replace setup.cfg by setup.py and tox.ini

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,8 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""The setup script. Configuration is in ``setup.cfg``."""
+"""
+The setup script.
+"""
 
 from setuptools import setup
+from setuptools import find_packages
 
-setup()
+def read_file(path):
+    with open(path, "r") as f:
+        return "\n".join([l.strip() for l in f.readlines()])
+
+description_files = ["README.rst", "HISTORY.rst"]
+
+metadata = {
+    "name": "{{ cookiecutter.project_slug }}",
+    "version": "{{ cookiecutter.version }}",
+    "description": "{{ cookiecutter.project_short_description }}",
+    "long_description": "\n\n".join([read_file(f) for f in description_files]),
+    "author": "{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    "author_email": "{{ cookiecutter.email }}",
+    "url": "https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}",
+    "keywords": "{{ cookiecutter.project_slug }}",
+    "classifiers": [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+}
+
+python = ">=3.7"
+
+dependencies = [
+    "Click >= 6.0",
+]
+
+scripts = [
+    '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
+]
+
+setup(
+    python_requires=python,
+    install_requires=dependencies,
+    entry_points={"console_scripts": scripts},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    **metadata,
+)
