28a91307701b4bb6e8bd305c402a629e8ed61598
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-10-28 13:32:50 +0100

    refine function to remove file/dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+# Standard library
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
 
 
 if __name__ == '__main__':
