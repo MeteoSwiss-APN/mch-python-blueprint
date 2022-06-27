0299e511f26187bd31956835e79cabcee8051e25
Daniel Regenass <daniel.regenass@meteoswiss.ch>
2022-06-27 12:06:40 +0200

    fix installation path in install script

--- {{cookiecutter.project_slug}}/install.sh
+++ {{cookiecutter.project_slug}}/install.sh
@@ -16,8 +16,8 @@ then
     echo "Found a conda executable at: ${CONDA_EXE}"
 else
     echo "No conda executable available, fetching Miniconda install script."
-    wget -O {$INSTALL_PREFIX}/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash miniconda.sh -b -p $WORKSPACE/miniconda
+    wget -O ${INSTALL_PREFIX}/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash ${INSTALL_PREFIX}/miniconda.sh -b -p ${INSTALL_PREFIX}/miniconda
     source ${CONDA_EXEDIR}/../etc/profile.d/conda.sh
     conda config --set always_yes yes --set changeps1 no
     conda config --add channels conda-forge
