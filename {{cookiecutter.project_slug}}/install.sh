04f8abaa23325fe7c79310c13024b6f9ff45508a
Daniel Regenass <daniel.regenass@meteoswiss.ch>
2022-06-27 12:36:40 +0200

    set path of conda.sh independent of conda env.

--- {{cookiecutter.project_slug}}/install.sh
+++ {{cookiecutter.project_slug}}/install.sh
@@ -18,10 +18,10 @@ else
     echo "No conda executable available, fetching Miniconda install script."
     wget -O ${INSTALL_PREFIX}/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash ${INSTALL_PREFIX}/miniconda.sh -b -p ${INSTALL_PREFIX}/miniconda
-    source ${CONDA_EXEDIR}/../etc/profile.d/conda.sh
+    source ${INSTALL_PREFIX}/miniconda/etc/profile.d/conda.sh
     conda config --set always_yes yes --set changeps1 no
     conda config --add channels conda-forge
     conda init bash
     conda activate
-    rm miniconda.sh
+    rm ${INSTALL_PREFIX}/miniconda.sh
 fi
