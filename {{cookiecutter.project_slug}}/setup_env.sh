de5ed7ee33d0b223c2c39a9eb1ba744ff6cd07f6
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-09-21 10:03:31 +0200

    add -c to set change command (for mamba); add defaults to and fix help

--- {{cookiecutter.project_slug}}/setup_env.sh
+++ {{cookiecutter.project_slug}}/setup_env.sh
@@ -12,6 +12,19 @@ PYVERSION=3.10
 DEV=false
 PINNED=false
 FORCE=false
+CONDA=conda
+
+help="Usage: $(basename "${0}") [-n NAME] [-p VER] [-d] [-p] [-c] [-f] [-c CMD] [-h]
+
+Options:
+ -n NAME    Name of the environment [default: ${ENV_NAME}]
+ -p VER     Desired Python version [default: ${PYVERSION}]
+ -d         Dev (editable) installation with additional dependencies
+ -p         Pinned installation with fully fixed dependencies
+ -f         Force overwrite of existing env (otherwise interactive)
+ -c CMD     Conda command [default: ${CONDA}]
+ -h         Print this help message and exit
+"
 
 # Eval command line options
 while getopts n:v:dph flag; do
@@ -21,50 +34,42 @@ while getopts n:v:dph flag; do
         d) DEV=true;;
         p) PINNED=true;;
         f) FORCE=true;;
+        c) CONDA=${OPTARG};;
         h) HELP=true;;
     esac
 done
 
 if ${HELP}; then
-    echo "Usage: $(basename "${0}") [-n <env_name>] [-v <python_version>] [-d] [-p] [-c] [-h]
-
-    Options:
-     -n Name of the environment
-     -v Desired Python version
-     -d Dev (editable) installation with additional dependencies
-     -p Pinned installation with fully fixed dependencies
-     -f Force overwrite of existing env (otherwise interactive)
-     -h Print this help message and exit
-    "
+    echo "${help}"
     exit 0
 fi
 
 echo "Setting up environment for installation"
-eval "$(conda shell.bash hook)"
-conda activate
+eval "$(conda shell.bash hook)"  # NOT ${CONDA} (doesn't work with mamba)
+conda activate  # NOT ${CONDA}
 
 # Create new env; pass -f to overwriting any existing one
-echo "Creating conda environment"
-conda create -n ${ENV_NAME} $(${FORCE} && echo --yes)
-conda install -n ${ENV_NAME} python=${PYVERSION} --yes
+echo "Creating ${CONDA} environment"
+${CONDA} create -n ${ENV_NAME} $(${FORCE} && echo --yes)
+${CONDA} install -n ${ENV_NAME} python=${PYVERSION} --yes
 
 # Install requirements in new env
 if ${PINNED}; then
     echo "Pinned installation"
     if ! ${DEV}; then
         echo "Prod installation"
-        conda env update --name ${ENV_NAME} --file requirements/environment.yml
+        ${CONDA} env update --name ${ENV_NAME} --file requirements/environment.yml
     else
         echo "Dev installation"
-        conda env update --name ${ENV_NAME} --file requirements/dev-environment.yml
+        ${CONDA} env update --name ${ENV_NAME} --file requirements/dev-environment.yml
     fi
 else
     echo "Unpinned installation"
-    conda env update --name ${ENV_NAME} --file requirements/requirements.yml
+    ${CONDA} env update --name ${ENV_NAME} --file requirements/requirements.yml
     if ! ${DEV}; then
         echo "WARNING: Unpinned prod installation!!!" >&2
     else
         echo "Dev installation"
-        conda env update --name ${ENV_NAME} --file requirements/dev-requirements.yml
+        ${CONDA} env update --name ${ENV_NAME} --file requirements/dev-requirements.yml
     fi
 fi
