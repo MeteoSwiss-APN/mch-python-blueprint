30bb654bcbfe3d276a95fe344754220e871e8210
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2023-01-12 18:01:58 +0100

    add option

--- tmpl/setup_miniconda.sh
+++ tmpl/setup_miniconda.sh
@@ -8,17 +8,22 @@
 
 # Default options
 INSTALL_PREFIX=${PWD}
+USER_INSTALL=true
 
 # here the conda version is fixed, the sha256 hash has to be set accordingly
 MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 SHA256=00938c3534750a0e4069499baf8f4e6dc1c2e471c86a59caa0dd03f4a9269db6
 
 # Eval command line options
-while getopts p: flag; do
+while getopts p:n  flag; do
     case ${flag} in
         p) INSTALL_PREFIX=${OPTARG};;
+        n) USER_INSTALL=false;;
     esac
 done
+
+echo $INSTALL_PREFIX
+echo $USER_INSTALL
 
 # Install conda executable if not yet available
 if [[ -f $CONDA_EXE ]]; then
@@ -31,7 +36,12 @@ else
     source ${INSTALL_PREFIX}/miniconda/etc/profile.d/conda.sh
     conda config --set always_yes yes --set changeps1 no
     conda config --add channels conda-forge
-    conda init bash
+    if ${USER_INSTALL}; then
+      conda init bash
+    else
+      # this is a workaround as plain --no-user is not working as it should
+      conda init bash --no-user --install --system
+    fi
     conda activate
     rm ${INSTALL_PREFIX}/miniconda.sh
 fi
