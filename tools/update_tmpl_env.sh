aa3272a48d4a09022c7305d0e96a26e8159312e4
Stefan Rüdisühli <stefan.ruedisuehli@env.ethz.ch>
2023-01-18 11:44:49 +0100

    Merge dev-requirements into requirements (#87)
    * merge dev-requirements into requirements files and adapt scripts
    
    * move reqs and env files into root
    
    * move reqs and env files into root; adapt script; update envs
    
    * move scripts into tools dir
    
    * add scripts to create envs for root or tmpl
    
    * fix remaining references to dev-requirements in scripts and docs
    
    * fix remaining references to dev-requirements in scripts and docs
    
    * add missing export flag
    
    * move unified pip section to end and update env
    
    * move unified pip section to end and update env
    
    * add missing tools/ in scripts and docs
    
    * move tools scripts under tool dir
    
    * call script explicitly with bash
    
    * call script explicitly with bash
    
    * add missing tools/
    
    * add missing tools/
    
    * reorder some lines and repeat 'repo: local' for each local hook to visually separate them better
    
    * change language of local hooks to system and remove additional_dependencies
    
    * remove unnecessary require_serial
    
    * clean up descriptions
    
    * reorder options
    
    * add checkers for forbidden files (.rej, .orig)
    
    * some cleanup and minor tweaks
    
    * add proper rstcheck config
    
    * add codespell config (and necessary dependency tomli to read pyproject.toml)
    
    * reorder linter hooks
    
    * clean up files options
    
    * don't pass (unnecessary) file names to run-mypy.sh
    
    * add comment regarding potential run-linters.sh script
    
    * increase min. versions of rstcheck and pydocstyle so they find their config in pyproject.toml
    
    * remove trailing newline in .copier-answers.yml
    
    * fix min version of codespell
    
    * fix blueprint pre-commit hooks and configs
    
    * add language to code-block
    
    * add toml support to rstcheck
    
    * clean up descriptions of requirements and environment files
    
    * move requirements and environment files back into requirements/ to facilitate copier update
    
    * fix references to setup_env.sh in docs/scripts
    
    * fix typo in jenkinsfile
    
    * Update installation.rst.j2
    
    * revise installation docs
    
    * move requirements and environment files back into requirements/ (as in branch unify-reqs)
    
    * merge changes from branch tools
    
    * merge changes from branch unify-reqs
    
    * merge changes from branch tools
    
    * Clean up copier params (#89)
    
    * reorder params
    
    * adapt param descriptions
    
    * change project_github from partial to full github url
    
    * add param project_keywords
    
    * add params project_ssh_url and project_doc_url
    
    * add better names as comments (in case ever renamed)
    
    * set default GitHub user to MeteoSwiss-APN
    
    Co-authored-by: Dani <daniel.regenass@meteoswiss.ch>
    
    Co-authored-by: Dani <daniel.regenass@meteoswiss.ch>

---
+++ tools/update_tmpl_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+# update_tmpl_env.sh
+# Create a default package and update the project's environment file
+
+
+main()
+{
+    # Create a temporary default project
+    local tmpdir="_tmp"
+    if [[ -d "${tmpdir}" ]]; then
+        \rm -rfv "${tmpdir}" || return
+    fi
+    copier -f --vcs-ref=HEAD . "${tmpdir}" || return
+
+    # Update the project's environment file
+    \cd "${tmpdir}" || return
+    local env_name="flying-circus"
+    bash -x tools/setup_env.sh -n "${env_name}" -ue || return
+    conda env remove -n "${env_name}" || return
+    \cd .. || return
+
+    # Merge updated environment file back into template
+    \cp -v "${tmpdir}"/requirements/environment.yml tmpl/requirements/environment.yml.j2 || return
+    \sed -i 's/^name:.*/name: {{ project_slug }}/' tmpl/requirements/environment.yml.j2 || return
+
+    # Clean up
+    \rm -rv "${tmpdir}" || return
+}
+
+
+main "${@}"
